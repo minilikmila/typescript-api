b66303c8c9416d99883a5afa2b0c58010aab3598
Minilik Shumete <minilikshumete@gmail.com>
2023-08-21 00:08:20 +0300

    add routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,22 @@
 import express, { Router } from "express";
-import { register, login } from "../controllers/auth";
+import {
+  register,
+  login,
+  confirmPhoneNumber,
+  resendPhoneConfirmationCode,
+  phoneForgotPassword,
+  resetPassword,
+} from "../controllers/auth";
+import { validatePConfirmation } from "../middleware/check_p_code";
 
 const router: Router = express.Router();
 
-router.post("/register", register).post("/login", login);
+router
+  .post("/register", register)
+  .post("/login", login)
+  .post("/confirm_phone", validatePConfirmation, confirmPhoneNumber)
+  .get("/resend_phone_code", validatePConfirmation, resendPhoneConfirmationCode)
+  .post("/forgot_password", phoneForgotPassword)
+  .post("/reset_password", resetPassword);
 
 export default router;
