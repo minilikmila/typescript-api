041a8c6815f46f17c7b11126a5f96879e1e92f97
Minilik Shumete <minilikshumete@gmail.com>
2023-08-21 00:10:38 +0300

    settings on user model

--- src/model/user.ts
+++ src/model/user.ts
@@ -11,10 +11,41 @@ const userSchema = new Schema<UserSchemaType>(
     email: {
       type: String,
       required: [true, "Email is required field!"],
+      unique: true,
     },
     password: {
       type: String,
       required: [true, "Password is required field!"],
+    },
+    is_email_confirmed: {
+      type: Boolean,
+      default: false,
+    },
+    is_phone_confirmed: {
+      type: Boolean,
+      default: false,
+    },
+    phone_confirmation_code: {
+      type: String,
+      required: false,
+    },
+    email_confirmation_token: {
+      type: String,
+      required: false,
+    },
+    email_confirmed_at: {
+      type: Date,
+    },
+    phone_confirmed_at: {
+      type: Date,
+    },
+    phone_number: {
+      type: String,
+      required: [true, "Phone number is required field."],
+    },
+    country_code: {
+      type: String,
+      required: true,
     },
   },
   {
