c666c1ae3da89caa44ffc9a73ff4d4ddc903b258
Minilik Shumete <minilikshumete@gmail.com>
2023-08-22 16:12:34 +0300

    add custom validator to routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,12 @@
 import express, { Router } from "express";
+import { validate } from "../middleware/validation_middleware";
+import {
+  authorization,
+  authPassword,
+  emailAddress,
+  phoneNumber,
+} from "../validator/auth_validator";
+
 import {
   register,
   login,
@@ -15,15 +23,20 @@ import { validatePConfirmation } from "../middleware/check_p_code";
 const router: Router = express.Router();
 
 router
-  .post("/register", register)
-  .post("/login", login)
+  .post("/register", validate([emailAddress(), authPassword()]), register)
+  .post("/login", validate([authorization()]), login)
   .post("/confirm_phone", validatePConfirmation, confirmPhoneNumber)
   .get("/resend_phone_code", validatePConfirmation, resendPhoneConfirmationCode)
-  .post("/forgot_password", phoneForgotPassword)
-  .post("/reset_password", resetPassword)
+  .post("/forgot_password", validate([phoneNumber()]), phoneForgotPassword)
+  .post("/reset_password", validate([phoneNumber()]), resetPassword)
   .post("/request_otp_authentication_code", otpAuthenticationRequest)
   .post("/otp_authentication", otpAuthentication)
-  .post("/confirm_email", confirmEmail);
+  .post(
+    "/confirm_email",
+    validate([phoneNumber()]),
+    [emailAddress()],
+    confirmEmail
+  );
 //   refresh token
 
 export default router;
--- src/routes/goal.ts
+++ src/routes/goal.ts
@@ -2,13 +2,15 @@ import express, { Router } from "express";
 
 import { SetGoal, GetGoals, DeleteGoal, UpdateGoal } from "../controllers/goal";
 import { Verify } from "../middleware/check_auth";
+import { authorization } from "../validator/auth_validator";
+import { validate } from "../middleware/validation_middleware";
 
 const router: Router = express.Router();
 
 router
-  .post("/", Verify, SetGoal)
-  .get("/", Verify, GetGoals)
-  .delete("/:id", Verify, DeleteGoal)
-  .patch("/:id", Verify, UpdateGoal);
+  .post("/", validate([authorization()]), Verify, SetGoal)
+  .get("/", validate([authorization()]), Verify, GetGoals)
+  .delete("/:id", validate([authorization()]), Verify, DeleteGoal)
+  .patch("/:id", validate([authorization()]), Verify, UpdateGoal);
 
 export default router;
